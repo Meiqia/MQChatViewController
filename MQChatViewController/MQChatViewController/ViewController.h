3c40e31e88b597cb46fcceac3e75350e01c3c926
ijinmao <340052204@qq.com>
2015-10-30 16:19:37 +0800

    first commit

---
+++ MQChatViewController/MQChatViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQChatViewController
+//
+//  Created by ijinmao on 15/10/30.
+//  Copyright © 2015年 ijinmao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
