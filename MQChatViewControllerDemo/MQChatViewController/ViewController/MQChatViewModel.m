4352700cf5fab998b6103be24a17a10c12542e37
ijinmao <340052204@qq.com>
2015-10-31 18:05:12 +0800

    add messageDateCell generate function in chatViewModel

--- MQChatViewControllerDemo/MQChatViewController/ViewController/MQChatViewModel.m
+++ MQChatViewControllerDemo/MQChatViewController/ViewController/MQChatViewModel.m
@@ -14,10 +14,13 @@
 #import "MQTextCellModel.h"
 #import "MQImageCellModel.h"
 #import "MQVoiceCellModel.h"
+#import "MQMessageDateCellModel.h"
 
 #ifdef INCLUDE_MEIQIA_SDK
 #import "MQManager.h"
 #endif
+
+static NSInteger const kMQChatMessageMaxTimeInterval = 60;
 
 /** 一次获取历史消息数的个数 */
 static NSInteger const kMQChatGetHistoryMessageNumber = 20;
@@ -57,6 +60,7 @@ static NSInteger const kMQChatGetHistoryMessageNumber = 20;
 - (void)sendTextMessageWithContent:(NSString *)content {
     MQTextMessage *message = [[MQTextMessage alloc] initWithContent:content];
     MQTextCellModel *cellModel = [[MQTextCellModel alloc] initCellModelWithMessage:message cellWidth:self.chatViewWidth];
+    [self generateMessageDateCellWithCurrentCellModel:cellModel];
     [self.cellModels addObject:cellModel];
 #ifdef INCLUDE_MEIQIA_SDK
     [MQManager sendTextMessageWithContent:content delegate:self];
@@ -70,6 +74,7 @@ static NSInteger const kMQChatGetHistoryMessageNumber = 20;
 - (void)sendImageMessageWithImage:(UIImage *)image {
     MQImageMessage *message = [[MQImageMessage alloc] initWithImage:image];
     MQImageCellModel *cellModel = [[MQImageCellModel alloc] initCellModelWithMessage:message cellWidth:self.chatViewWidth];
+    [self generateMessageDateCellWithCurrentCellModel:cellModel];
     [self.cellModels addObject:cellModel];
 #ifdef INCLUDE_MEIQIA_SDK
     [MQManager sendImageMessageWithImage:image delegate:self];
@@ -82,6 +87,7 @@ static NSInteger const kMQChatGetHistoryMessageNumber = 20;
 - (void)sendVoiceMessageWithVoice:(NSData *)voiceData {
     MQVoiceMessage *message = [[MQVoiceMessage alloc] initWithVoiceData:voiceData];
     MQVoiceCellModel *cellModel = [[MQVoiceCellModel alloc] initCellModelWithMessage:message cellWidth:self.chatViewWidth];
+    [self generateMessageDateCellWithCurrentCellModel:cellModel];
     [self.cellModels addObject:cellModel];
 #ifdef INCLUDE_MEIQIA_SDK
     [MQManager sendAudioMessage:voiceData delegate:self];
@@ -110,6 +116,35 @@ static NSInteger const kMQChatGetHistoryMessageNumber = 20;
 
 - (void)didSendMessage:(MQMessage *)message expcetion:(kMQExceptionStatus)expcetion {
     
+}
+
+/**
+ * 判断两个时间间隔是否过大，如果过大，返回一个MessageDateCellModel
+ */
+- (void)generateMessageDateCellWithCurrentCellModel:(id<MQCellModelProtocol>)currentCellModel {
+    NSDate *lastDate = [self getBussinessCellModelDateWithIndex:self.cellModels.count];
+    NSDate *nextDate = [currentCellModel getCellDate];
+    //如果上一个cell的时间比下一个cell还要大（说明currentCell是第一个业务cell，此时显示时间cell）
+    BOOL isLastDateLargerThanNextDate = lastDate.timeIntervalSince1970 > nextDate.timeIntervalSince1970;
+    //如果下一个cell比上一个cell的时间间隔没有超过阈值
+    bool isDateTimeIntervalSmallerThanThreshold = nextDate.timeIntervalSince1970 - lastDate.timeIntervalSince1970 < kMQChatMessageMaxTimeInterval;
+    if (!isLastDateLargerThanNextDate && isDateTimeIntervalSmallerThanThreshold) {
+        return ;
+    }
+    MQMessageDateCellModel *cellModel = [[MQMessageDateCellModel alloc] initCellModelWithDate:nextDate cellWidth:self.chatViewWidth];
+    [self.cellModels addObject:cellModel];
+}
+
+/**
+ * 从cellModels中获取到业务相关的cellModel的时间，即text, image, voice的时间；
+ */
+- (NSDate *)getBussinessCellModelDateWithIndex:(NSInteger)index {
+    id<MQCellModelProtocol> cellModel = [self.cellModels objectAtIndex:index];
+    if ([cellModel isKindOfClass:[MQTextCellModel class]] || [cellModel isKindOfClass:[MQImageCellModel class]] || [cellModel isKindOfClass:[MQVoiceCellModel class]]){
+        return [cellModel getCellDate];
+    }
+    [self getBussinessCellModelDateWithIndex:index - 1];
+    return [NSDate date];
 }
 
 
